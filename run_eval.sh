71d17f2eeb333a2f7369cfe099c33df2dab9097e
jiayin <jiayin-l23@mails.tsinghua.edu.cn>
2026-03-27 08:16:36 -0400

    eval

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,13 @@
-cd src
-python -m hgan.eval \
-  --config-path hgan/configuration.ini \
-  --output-folder ../eval_results \
-  --system_name pendulum \
-  --perplexity 2 5 30 50
-cd ..
+#!/bin/bash
+set -e
+
+BASE_DIR="/home/jiayinliu/projects/SympNet"
+
+cd "${BASE_DIR}"
+python scripts/eval_all_systems.py \
+  --base-dir "${BASE_DIR}" \
+  --config-name configuration_60.ini \
+  --output-dir "${BASE_DIR}/eval_results_all_systems" \
+  --num-trajectory-samples 100 \
+  --latent-batch-size 1024 \
+  --eval-batch-size 64
